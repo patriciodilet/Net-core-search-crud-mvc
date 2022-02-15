61bdac44264c985770cc230a0d8b4ed79120e06d
Patricio Diaz <patricio.diaz@tecnova.cl>
2022-02-15 20:56:13 -0300

    Agregar archivos de proyecto.

---
+++ Net-core-search-crud-mvc/Models/NewEmpClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Net_core_search_crud_mvc.Models
+{
+    public class NewEmpClass
+    {
+        [Key]
+        public int EmpId { get; set; }
+        
+        [Required(ErrorMessage = "Enter Employee Name")]
+        [Display(Name = "Employee Name")]
+        public string EmpName { get; set; }
+
+        [Required(ErrorMessage = "Enter email")]
+        [Display(Name = "Email")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
+        
+        [Required(ErrorMessage = "Enter age")]
+        [Display(Name = "Age")]
+        [Range(20,50)]
+        public int Age { get; set; }
+        
+        [Required(ErrorMessage = "Enter salary")]
+        [Display(Name = "Salary")]
+        public int Salary { get; set; }
+
+    }
+}
